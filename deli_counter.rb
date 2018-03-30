3c9d5a55388e4e892d75713346cadbf4ee7e2fdd
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 18:32:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,13 @@ def line(katz_deli)
     puts msg
     return msg
   end
+  
+  msg = "The line is currently: "
   katz_deli.each_index do |e, i|
-    
+    p = i + 1
+    msg += p.to_s + " " + e.strip + " "
   end
-
+    msg = msg.strip
+    puts msg
+    msg
 end
