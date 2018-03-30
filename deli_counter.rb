1fe8bebdc86eead632c0f55fdcfe4bc6366e0b20
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 18:29:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,11 @@
 def line(katz_deli)
+  msg = "The line is currently empty."
   if katz_deli.size == 0
-    puts "The line is currently empty."
+    puts msg
+    return msg
   end
+  katz_deli.each_index do |e, i|
+    
+  end
+
 end
